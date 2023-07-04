c6559e5f914d960dc88a813333008137ebc27bee
joey-ammar <xyx8703@gmail.com>
2023-07-04 18:41:43 +0200

    variables Meta lesson

---
+++ Programming with JavaScript/variables.js
@@ -0,0 +1,5 @@
+let name = "john"; // name variable will hold the value john
+let greeting = "Hallo";
+console.log("🚀 ~ file: variables.js:3 ~ greeting:", greeting);
+console.log("🚀 ~ file: variables.js:2 ~ name:", name);
+console.log(greeting, name);
