2b651e49a8c4b16a72364cda8526b59cf36575c9
joey-ammar <xyx8703@gmail.com>
2023-07-05 09:00:04 +0200

    variables exercise

--- Programming with JavaScript/variables.js
+++ Programming with JavaScript/variables.js
@@ -3,3 +3,18 @@ let greeting = "Hallo";
 console.log("🚀 ~ file: variables.js:3 ~ greeting:", greeting);
 console.log("🚀 ~ file: variables.js:2 ~ name:", name);
 console.log(greeting, name);
+
+//Variables exercise
+
+let petDog = "Rex";
+let petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+let catSound = "purr";
+let dogSound = "woof";
+console.log(petDog + "says" + dogSound);
+console.log(petCat + "says" + catSound);
+catSound = "meow";
+console.log(petCat + "now says" + catSound);
