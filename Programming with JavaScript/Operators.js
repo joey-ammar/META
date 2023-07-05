91228cc0732b12758d972da94a2392b9fbd58369
joey-ammar <xyx8703@gmail.com>
2023-07-05 09:57:34 +0200

    Operators in javascript

---
+++ Programming with JavaScript/Operators.js
@@ -0,0 +1,34 @@
+/** Assign Operator = */
+let person = "Jim";
+/**Arithmetic Operator */
+/**
+ *  + Addition
+ *  - Subtraction
+ *  / Division
+ *  * Multiplication
+ */
+console.log(2 + 25 + 2);
+console.log(18 - 23);
+console.log(4 / 2);
+console.log(2 * 2);
+/**
+ * Comparison Operator
+ * > Greater than 3 > 2
+ * < Less than 2 < 3
+ * == equal to
+ * != Not equal to
+ */
+
+console.log(3 > 2);
+console.log(3 < 2);
+console.log(3 == 2);
+console.log(3 != 3);
+
+/** Logical Operator */
+/**
+ *  && checks for both conditions to be true
+ *  || checks for at least one condition is true ( returns the first true)
+ *  ! returns false of the condition is true
+ */
+console.log(3 && 3);
+console.log(12 || 13);
