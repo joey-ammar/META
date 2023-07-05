b08c4bb57e9f05db02af532ca7186a4d9f96623a
joey-ammar <xyx8703@gmail.com>
2023-07-05 09:15:39 +0200

    DataType explanation

---
+++ Programming with JavaScript/DataTypes.js
@@ -0,0 +1,30 @@
+// primitive Data Type
+/**
+ * 1 - String
+ * 2 - Number
+ * 3 - Boolean
+ * 4 - Null
+ * 5 - Undefined
+ * 6 - Bignit
+ * 7 - Symbol
+ */
+
+/** String */
+let myString = "Hallo World";
+/** Number */
+let myNumber = 12213;
+/** Boolean */
+let myBoolean = true;
+let myFalseBoolean = false;
+/** Null (absence of data) */
+let myNull = null;
+console.log("🚀 ~ file: DataTypes.js:21 ~ myNull:", myNull);
+/* Undefined ( A variable not assigned to a value yet) */
+let undefinedValue;
+console.log("🚀 ~ file: DataTypes.js:24 ~ undefinedValue:", undefinedValue);
+/**Big Int */
+let myBigInt = 9007199254740991n;
+console.log("🚀 ~ file: DataTypes.js:27 ~ myBigInt:", myBigInt);
+/**Symbol (unqiue identifier) */
+let sym2 = Symbol("foo");
+console.log("🚀 ~ file: DataTypes.js:30 ~ sym2:", sym2);
